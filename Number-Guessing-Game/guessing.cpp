c77fe7c0cc1beea74976764b9e85f960d4877b84
Utku Akgül <utkuakgul3453@gmail.com>
2024-01-22 22:21:32 +0300

    Create guessing.cpp

---
+++ Number-Guessing-Game/guessing.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+
+
+int main()
+{
+
+
+    int num;
+    int guess;
+    int tries = 0;
+
+
+    srand(time(NULL));
+    num = (rand() % 100) + 1;
+
+
+    std::cout << "************** NUMBER GUESSING GAME *****************";
+
+    do{
+        std::cout << "Enter a guess between (1-100): ";
+        std::cin >> guess;
+        tries++;
+
+        if(guess > num){
+            std::cout << "Too high! \n";
+        }
+
+        else if(guess < num){
+            std::cout << "Too low! \n";
+        }
+
+        else{
+            std::cout << "CORRECT !!!! # of tries: " << tries << "\n";
+        }
+
+    }while (guess != num);
+
+    std::cout << "************* NUMBER GUESSING GAME ********** \n";
+
+    return 0;
+
+
+
+
+} 
